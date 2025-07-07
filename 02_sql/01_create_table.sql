e4360b41e4d386a80c958256932111a804b62b4a
Ngan Huynh <146058636+krishuynh2222@users.noreply.github.com>
2025-07-06 21:35:32 -0400

    Create 01_create_table.sql

---
+++ 02_sql/01_create_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE fair_price_data (
+    Item_Fat_Content VARCHAR(50),
+    Item_Identifier VARCHAR(50),
+    Item_Type VARCHAR(50),
+    Outlet_Establishment_Year INT,
+    Outlet_Identifier VARCHAR(50),
+    Outlet_Location_Type VARCHAR(50),    
+    Outlet_Size VARCHAR(50),
+    Outlet_Type VARCHAR(50),
+    Item_Visibility FLOAT,
+    Item_Weight FLOAT NULL,
+    Sales FLOAT,    
+    Rating FLOAT
+);
